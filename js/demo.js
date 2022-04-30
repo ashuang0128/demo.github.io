c778e0e1af261fb673660b96ad8038a05cabcbcd
ashuang0128 <johnny0920799@gmail.com>
2022-05-01 04:53:39 +0800

    add

--- js/demo.js
+++ js/demo.js
@@ -1,3 +1,8 @@
 var xhr = new XMLHttpRequest()
-xhr.open("Get","./info.json",true)
+xhr.onreadystatechange = function(){
+    if(xhr.readyState == 4 && xhr.status == 200){
+        document.getElementById("test").innerHTML = xhr.response
+    }
+}
+xhr.open("GET","./info.json",true)
 xhr.send()
